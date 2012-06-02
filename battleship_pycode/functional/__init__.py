b98434e39c6033fbebb2b7720613d4d356e94ea0
christian <christian@linux-06wv.site>
2012-06-02 16:56:30 +0200

    23

---
+++ battleship_pycode/functional/__init__.py
@@ -0,0 +1,8 @@
+'''
+Created on Jun 2, 2012
+
+@author: christian
+'''
+
+if __name__ == '__main__':
+    pass
---
+++ battleship_pycode/functional/players.py
@@ -0,0 +1,17 @@
+'''
+Created on Jun 2, 2012
+
+@author: christian
+'''
+
+class MyClass( object ):
+    '''
+    classdocs
+    '''
+
+
+    def __init__( self ):
+        '''
+        Constructor
+        '''
+        
