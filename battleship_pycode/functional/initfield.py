8bc6b542573478d25128e0d8bbe658334469d589
christian <christian@linux-06wv.site>
2012-06-04 01:22:35 +0200

    :?

--- battleship_pycode/functional/initfield.py
+++ battleship_pycode/functional/initfield.py
@@ -21,19 +21,21 @@ import numpy as np
    # ToDo: Comes here....
    # - make array field 10x10 //check
    # - 
-   # -
+   # -####self.mut_array_player1 = matrix10x10 = [[0 for height in range(10)] for width in range(10)]#####
    #============================================================================
 class MATRIX( object ):    
     def __init__( self, height = 10, width = 10 ):
-        self.mut_array = np.zeros( ( height, width ), dtype = int )
-        self.buf_array = np.ones( ( height, width ), dtype = int )
-        
+        self.Height = height
+        self.Width = width
+        self.mut_array_player1 = np.zeros( ( height, width ), dtype = int )
+        self.mut_array_player2 = np.zeros( ( height, width ), dtype = int )
+         
     def fill_array( self, array_dimension = (), x = 0, y = 0 ):
-        for y in range( array_dimension[0] ):
-#            self.mut_array[y][x] = 2
-            for x in range( array_dimension[1] ):
-                self.mut_array[y][x] = 2 
-        print( "array filled", self.mut_array )   
+        for i1 in range( array_dimension[0] ):
+            for i2 in range( array_dimension[1] ):
+                self.mut_array_player1[i1][i2] = 2
+        print( "array filled\n", self.mut_array_player1 )
+
     #===========================================================================
     # operation=0 (get the array it self)
     # operation=1 (return specific field)
@@ -41,54 +43,59 @@ class MATRIX( object ):
     # operation=3 (return specific column; need get_col argument)
     #===========================================================================
     def get_info( self, operation = 0, get_row = 0, get_col = 0 ):
+        
         if operation == 1:
-            return self.mut_array[get_row][get_col]
+            return self.mut_array_player1[get_row][get_col]
         elif operation == 2:
             print( "second get 'array col'\n" )
-            return self.mut_array[:, get_col]
+            return self.mut_array_player1[:, get_col]
         elif operation == 3:
-            return self.mut_array[:]
-        
-        dim_array = self.mut_array.shape
-        return dim_array
+            return self.mut_array_player1[:]
+        elif operation == 4:
+            for y in range( self.Height ):
+                for x in range( self.Width ):
+                    check = self.mut_array_player2[y][x]
+                    if check == -1:
+                        self.mut_array_player1[y][x] = 0
+                        #player mist the ship
+                    elif check == 0:
+                        self.mut_array_player1[y][x] = 3
+                        pass
+                        # nothing happens
+                    elif check == 1 :
+                        pass
+                        #ship has been shot
+                    elif check == 2:
+                        self.mut_array_player1[y][x] = -1
+                        pass
+                        #ship ok
+            print( self.mut_array_player1 )
+        else:
+            return  self.mut_array_player1.shape ##dimension
     
     def get_array( self ):
-        return self.mut_array
+        return self.mut_array_player1
+    
+    def tester( self ):
+        for i in range( 10 ):
+            self.mut_array_player2[i][i] = 2
+            
+            
 
 
 
-creat_matrix = MATRIX( 4, 4 )
-a = creat_matrix.get_info()
-print( 'first "get info"\n', a )
-creat_matrix.fill_array( a )
-a = creat_matrix.get_array()
-print( creat_matrix.get_info( 2, get_col = 1 ).reshape( 4, 1 ) ) 
 
 
-#------------------------------------------------------------------------------ OPTION TWO without numpy library--------------------------------------------------------
-#print( creat_matrix._fill_array() )
+creat_matrix = MATRIX( 10, 10 )
+print( "dim\n", creat_matrix.get_info() )
 
+creat_matrix.fill_array( creat_matrix.get_info() )
+creat_matrix.fill_array( creat_matrix.get_info() )
+creat_matrix.tester()
+creat_matrix.get_info( 4 )
 
-# 
-#class MATRIX( object ):
-#    def __init__( self, height = 10, width = 10 ):
-#        self.Height = width
-#        self.Width = height
-#        self.mut_array = matrix10x10 = [[0 for height in range(10)] for width in range(10)]
-#        
-#    def fill_array( self, array = 10 ):
-#        i1 = 0
-#        i2 = 0
-#        while i1 < self.Height:
-#            
-#        
-#        self.mut_array = np_zeros
-#
-#    def information( self ):
-#        dim_array = self.mut_array.shape
-#        return dim_array
-#    def get_array( self ):
-#        return self.mut_array
-#
-#creat_matrix = MATRIX( 4, 4 )
-#a = creat_matrix.fill_array()
+#print( 'first "get info"\n', a )
+#creat_matrix.fill_array( a )
+#a = creat_matrix.get_array()
+#print( creat_matrix.get_info( 2, get_col = 1 ).reshape( 10, 1 ) ) 
+
