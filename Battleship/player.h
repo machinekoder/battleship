be53cca79e1b711bb59b15d0dd31341fc96010c4
christian <christian@linux-06wv.site>
2012-07-16 23:08:54 +0200

    asdf

--- Battleship/player.h
+++ Battleship/player.h
@@ -38,13 +38,8 @@ public:
     explicit Player(QObject *parent, QString name, QString color, int fieldSize);
 
     void statistic();
-<<<<<<< HEAD
     bool playerShoot(int x, int y, int shootType);
-    void playerShipDestroyed(QPoint coordinates);
-=======
-    bool playerShoot(int x, int y, int shotType);
     bool playerShipDestroyed(QPoint coordinates);
->>>>>>> bc66d64c8a2ec138fb73c29d2e2e88449d2fb6c0
     void playerShootContinue(int x, int y);
     void computerPlaceShip();
     bool computerKi();
